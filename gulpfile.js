0143c09bc9f03c6e154cd671faac1ea5bc5a38b6
kripton-perm <mixail1024070@mail.ru>
2017-11-14 13:22:29 +0500

    Поправил настройки проекта

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,46 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var gcmq = require('gulp-group-css-media-queries');
+var sourcemaps = require('gulp-sourcemaps');
+var config = {
+    src: './src',
+    css: {
+        watch: '/sass/**/*.scss',
+        src: '/sass/styles.scss',
+        dest: '/css'
+    },
+    html: {
+        src: '/*.html'
+    }
+};
 
 gulp.task("style", function() {
-  gulp.src("sass/style.scss")
+  gulp.src(config.src + config.css.src)
+    //.pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass())
+    .pipe(gcmq())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    //.pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(config.src + config.css.dest))
+    .pipe(server.reload({
+      stream: true
+    }));
 });
 
 gulp.task("serve", ["style"], function() {
   server.init({
-    server: ".",
-    notify: false,
+    server: config.src,
+    //notify: false,
     open: false,
-    cors: true,
-    ui: false
+    cors: true//,
+    //ui: false
   });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch(config.src + config.css.watch, ["style"]);
+  //gulp.watch(config.src + config.html.src).on("change", server.reload);
+  gulp.watch(config.src + config.html.src, server.reload);
 });
