3a01d19f9032100d51a9a3a8f4f019a845479cae
kripton-perm <mixail1024070@mail.ru>
2017-11-21 14:30:48 +0500

    Поправил плэйсхолдеры в Firefox; поправил z-index у всплывающих окон

--- src/js/src/subscribe.js
+++ src/js/src/subscribe.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
   var $buttonSignup = $('.button--signup');
   var $modalSignup = $('.signup');
   var $signupClose = $('.signup .signup__close');
+  var $pageHeader = $('.page-header');
   $buttonSignup.on('click', function() {
     $modalSignup.addClass('modal--show');
+    $pageHeader.css('z-index', '0');
   });
   $signupClose.on('click', function() {
     $modalSignup.removeClass('modal--show');
+    $pageHeader.css('z-index', '2');
   });
 });
